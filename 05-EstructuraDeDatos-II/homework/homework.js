83484e6421689982509b7f83427e22f1517343e5
MatiasDAppella <appellamatias@gmail.com>
2023-06-09 17:02:11 -0300

    resolved

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -10,9 +10,74 @@ Implementar la clase LinkedList, definiendo los siguientes métodos:
   search(isEven), donde isEven es una función que retorna true cuando recibe por parámetro un número par, busca un nodo cuyo valor sea un número par.
   En caso de que la búsqueda no arroje resultados, search debe retornar null.
 */
-function LinkedList() {}
+function LinkedList(){
+    this.head = null,
+    this._length = 0,
 
-function Node(value) {}
+    // add: añade un nuevo nodo al final de la lista.
+    this.add = function(v){
+        let newElement = new Node(v)
+        if (this._length === 0) this.head = newElement
+        else {
+            let current = this.head
+            while (current.next) current = current.next
+            current.next = newElement
+        }
+        this._length++
+    },
+
+    // remove: elimina el último nodo de la lista y retorna su valor.
+    this.remove = function(){
+        let e;
+        switch (this._length){
+            case 0:
+                return null
+                
+            case 1:
+                e = this.head.value
+                this.head = null
+                this._length--
+                return e
+
+            default:
+                let current = this.head
+                while (current.next.next) current = current.next
+                e = current.next.value
+                current.next = null
+                this._length--
+                return e
+        }
+    },
+
+    // search: busca un nodo que coincida con el parametro ingresado.
+    //      1. concidera el valor del parametro, o;
+    //      2. concidera que el parametro es un cb que retorna true/false.
+    this.search = function(x){
+        let current = this.head
+        switch (typeof x){
+            case 'function':
+                let callback = x
+                for (let i = 0; i < this._length; i++){
+                    if (callback(current.value)) return current.value
+                    else current = current.next
+                }
+                break;
+
+            default:
+                for (let i = 0; i < this._length; i++){
+                    if (x == current.value) return current.value
+                    else current = current.next
+                }
+                break;
+        }
+        return null
+    }
+}
+
+function Node(value){
+    this.value = value,
+    this.next = null
+}
 
 /* EJERCICIO 2
 Implementar la clase HashTable.
@@ -27,7 +92,46 @@ La clase debe tener los siguientes métodos:
 
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
-function HashTable() {}
+class HashTable {
+    constructor(){
+        this.numBuckets = 35
+        this.buckets = new Array(this.numBuckets)
+    };
+
+    // hash: hashea el input y determina en que bucket se almacenará.
+    hash(input){
+        let bucket = 0;
+        for (let i = 0; i < input.length; i++){
+            bucket += input.charCodeAt(i)
+        }
+        return bucket % this.numBuckets
+    };
+
+    // set: recibe el objeto 'clave' y 'valor' (como dos parametros distintos),
+    //      hashea la clave invocando el metodo .hash() y almacena el dato.
+    set(key, value){
+        if (typeof key !== 'string') throw new TypeError('Keys must be strings')
+        let i = this.hash(key)
+        if (this.buckets[i] == undefined) this.buckets[i] = new Object()
+        this.buckets[i][key] = value
+    };
+
+    // get: recibe una clave por parámetro, y busca el valor en el bucket correspondiente.
+    get(key){
+        if (this.hasKey(key)) {
+            let i = this.hash(key)
+            return this.buckets[i][key]
+        }
+    };
+
+    // hasKey: recibe una clave por parámetro, y devuelve;
+    //       - true si hay algo almacenado con esa clave.
+    //       - false si no hay nada almacenado con esa clave.
+    hasKey(key){
+        let i = this.hash(key)
+        return this.buckets[i].hasOwnProperty(key)
+    };
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
